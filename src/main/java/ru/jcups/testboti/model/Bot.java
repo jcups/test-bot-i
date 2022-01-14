8bfe2bd8224f399f96d3d5222fb41ffb9f4ab604
jcups <theizumkacatle@gmail.com>
2022-01-14 18:51:11 +0300

    add logMessage

--- src/main/java/ru/jcups/testboti/model/Bot.java
+++ src/main/java/ru/jcups/testboti/model/Bot.java
@@ -24,18 +24,26 @@ public class Bot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+        logMessage(update);
         try {
             updateHandler.handle(update, this);
-            this.execute(new SendMessage("311199801", "New message from user: " +
-                    update.getMessage().getChat().getFirstName() + " " +
-                    update.getMessage().getChat().getLastName() + ", " +
-                    update.getMessage().getChat().getUserName() + ". \nText: " +
-                    update.getMessage().getText()));
+            sendMessageToOwner(update);
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
     }
 
+    private void logMessage(Update update) {
+        System.out.printf("New message from %s with text %s\n",update.getMessage().getChat().getUserName(), update.getMessage().getText());
+    }
+
+    private void sendMessageToOwner(Update update) throws TelegramApiException {
+        this.execute(new SendMessage("311199801", "New message from user: " +
+                update.getMessage().getChat().getFirstName() + " " +
+                update.getMessage().getChat().getLastName() + ", " +
+                update.getMessage().getChat().getUserName() + ". \nText: " +
+                update.getMessage().getText()));
+    }
     @Override
     public String getBotUsername() {
         return botUsername;
